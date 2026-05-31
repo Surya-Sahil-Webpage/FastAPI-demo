fc34e0f576674ab09d01c8667796f99b398b885f
Surya Sahil <sahil.vadapalli@gmail.com>
2026-06-01 03:43:21 +0530

    my second commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from models import Product
 
 app = FastAPI()
 
@@ -7,9 +8,10 @@ def greet():
     return "Welcome to Telusko Trac"
 
 products = [
-    
+    Product(id=2, name="phone", description="budget phone", price=99, quantity=22),
+    Product(id=3, name="Pen", description="budget pen", price=9, quantity=2)
 ]
 
 @app.get("/products")
 def get_all_products():
-    return "all products"
+    return products
